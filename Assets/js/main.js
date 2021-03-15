d87528a1240d9a32f22eef7e0bf08a0197c5d40b
Fabio-Colombo <thesmallworld95@gmail.com>
2021-03-15 16:04:53 +0100

    Metà esercizio completo

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -7,13 +7,41 @@
 
 
 // Scrivo con un innerHTML 5 numeri generati casualmente(fun Randomnumber)
+var randomNumbers = []
+var userNumbers = []
+for (var i = 0; i < 5; i++) {
+  randomNumberInRange(1, 100)
+  randomNumbers.push(" " + randomNumberInRange(1, 100) + " ")
+}
 
+$("#random-number").text(randomNumbers);
 // Faccio partire un timer (setTimeout) di duration (30s)
   // Faccio scomparire l'array di numeri casuali dal documento
-
-// Con un ciclo (for,while) di 5 volte faccio inserire ogni
-// volta all'utente (prompt) un numero che ha visto precedentemente
-
+setTimeout(function () {
+$("#random-number").addClass("active")
+  setTimeout(function () {
+  // Con un ciclo (for,while) di 5 volte faccio inserire ogni
+  // volta all'utente (prompt) un numero che ha visto precedentemente
+    for (var i = 0; i < 5; i++) {
+    var userNumber = parseInt(prompt("Inserisci uno dei numeri visti in precedenza"))
+    userNumbers.push(" " + userNumber + " ")
+    $("#user-number").text(userNumbers);
+    }
+  })
+}, 30000)
 // Stampo nuovamente array numeri casuali e numeri dell'utente
   // Scrivo quali e quanti numeri sono stati indovinati
   // Es. (verdi numeri indovinati) rossi (errati)
+
+
+
+
+
+// FUNCTIONS
+function randomNumberInRange(min, max) {
+  if (isNaN(min) || isNaN(max)) {
+    console.log("Non sono numeri");
+  } else {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+}
