1bf52ac9163e7d67ff0fa6e2ae2f35ebc102c0ce
Fabio-Colombo <thesmallworld95@gmail.com>
2021-03-15 15:30:17 +0100

    Divisione esercizio in microproblemi

---
+++ Assets/js/main.js
@@ -0,0 +1,19 @@
+// La pagina espone 5 numeri generati casualmente.
+// Da li parte un timer di 30 secondi.
+// Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri
+// che ha visto precedentemente, tramite il prompt().
+// Dopo che sono stati inseriti i 5 numeri,
+// il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
+
+// Scrivo con un innerHTML 5 numeri generati casualmente(fun Randomnumber)
+
+// Faccio partire un timer (setTimeout) di duration (30s)
+  // Faccio scomparire l'array di numeri casuali dal documento
+
+// Con un ciclo (for,while) di 5 volte faccio inserire ogni
+// volta all'utente (prompt) un numero che ha visto precedentemente
+
+// Stampo nuovamente array numeri casuali e numeri dell'utente
+  // Scrivo quali e quanti numeri sono stati indovinati
+  // Es. (verdi numeri indovinati) rossi (errati)
